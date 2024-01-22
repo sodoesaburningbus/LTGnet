84199c285ac61b26d4ca0ea8d5b6f2c121f1a553
Christopher E. Phillips <cephillips574@gmail.com>
2024-01-22 08:46:09 -0600

    Update run_model.py

--- run_model.py
+++ run_model.py
@@ -15,32 +15,5 @@ net.load_model('classifier_2024-01-20_2127.pt')
 # Run predictions
 y_pred = net.predict(X)
 
-# Mask to only confident areas
-y_pred = np.squeeze(y_pred.detach().numpy())
-mask = (y_pred > 0.9) | (y_pred < 0.2)
-y_pred = y_pred[mask]
-y_test = y_test[mask]
-
-fn = open('nifty_rows.txt', 'w')
-rows = np.arange(100000)[mask]
-for r in rows:
-    fn.write(f'{r}\n')
-fn.close()
-
-# Print performance
-y_pred = np.array(np.round(y_pred), dtype='bool')
-y_test = np.array(np.squeeze(y_test), dtype='bool')
-
-tp = np.sum(y_pred & y_test)
-tn = np.sum((~y_pred) & (~y_test))
-fp = np.sum(y_pred & (~y_test))
-fn = np.sum((~y_pred) & y_test)
-
-# Print to log
-print(f'\nTrue Positives = {tp}')
-print(f'\nTrue Negatives = {tn}')
-print(f'\nFalse Positives = {fp}')
-print(f'\nFalse Negatives = {fn}')
-print(f'\nAccuracy = {(tp+tn)/(tp+tn+fp+fn)*100.0:.2f}%')
-print(f'\nPoD = {tp/(tp+fn)*100.0:.2f}%')
-print(f'\nFAR = {fp/(fp+tn)*100.0:.2f}%')
+# Use simple rounding to convert probabilistic prediction to True/False
+y_pred = np.array(np.round(np.squeeze(y_pred.detach().numpy())), dtype='bool')
